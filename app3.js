fcd28040f3980407c5deb9a03f6a2f570b334473
AElAmiri <el.amiri.abdelhay@gmail.com>
2020-08-27 11:01:05 +0100

    Lecture19

--- app3.js
+++ app3.js
@@ -5,8 +5,7 @@ angular.module('ControllerAsApp', [])
 .controller('ParentController1', ParentController1)
 .controller('ChildController1', ChildController1)
 .controller('ParentController2', ParentController2)
-.controller('ChildController2', ChildController2)
-;
+.controller('ChildController2', ChildController2);
 
 ParentController1.$inject = ['$scope'];
 function ParentController1($scope) {
@@ -15,41 +14,44 @@ function ParentController1($scope) {
   $scope.pc.parentValue = 1;
 };
 
+
 ChildController1.$inject = ['$scope'];
 function ChildController1($scope) {
 
   console.log("$scope.parentValue :", $scope.parentValue);
-  console.log($scope);
+  console.log("$scope :", $scope);
 
   $scope.parentValue = 5;
   console.log("Changed : $scope.parentValue  ======== 5");
   console.log("$scope.parentValue :", $scope.parentValue);
-  console.log($scope);
+  console.log("$scope :", $scope);
 
   console.log("$scope.pc.parentValue Before ======>", $scope.pc.parentValue);
   $scope.pc.parentValue = 5;
   console.log("$scope.pc.parentValue After ======>", $scope.pc.parentValue);
   console.log($scope);
 
-  console.log("$scope.$parent.parentValue = ", $scope.$parent.parentValue);
+  console.log("$scope.parent.parentValue :", $scope.$parent.parentValue);
+
+
 
 };
 
 
-// ParentController2.$inject = ['$scope'];
-function ParentController2($scope) {
-  var parent = this;
-  this.value = 1;
+// ParentController2.$inject = ['$scope']
+function ParentController2() {
+  var parentt = this;
+  parentt.Value = 1;
 };
 
 
-ChildController2.$inject = ['$scope'];
+ChildController2.$inject = ['$scope']
 function ChildController2($scope) {
-  var child = this;
-  child.value = 5;
+  var childddddd = this;
+  childddddd.Value = 5;
   console.log("ChildController2 $scope :", $scope);
-
 };
+
 
 
 
